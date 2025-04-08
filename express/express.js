cde869aaed321e72d6d1d5af3fb012951a1a3516
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-04-08 16:03:37 +0530

    created server

---
+++ express/express.js
@@ -0,0 +1,38 @@
+// srver provide data in form of json
+// json(javascript object notation) - lightweight, structured and organized data.
+//   const jsonString  = '{"name": "Ambika","age": 24,"city":"Deoria"}'
+// const jsonObject = JSON.parse(jsonString)    // convert json string to object
+// console.log(jsonObject);
+
+// const str = JSON.stringify(jsonObject)    // convert object to json string
+// console.log(str);
+
+// console.log(typeof str);
+// console.log(typeof jsonObject);
+
+
+
+// imagine a menu card in restaurant
+// customer - client, waiter - server, kitchen room - db
+// collection of all lists(menu card) - api
+// option in that list - endpoint
+
+
+// creating server in NodeJS using express package
+// localhost - refers to own computer(house of waiter)
+// port number - address of server(room of waiter in that house)
+
+const express = require('express');
+const app = express();
+
+// get method used to request data from server
+app.get('/',(req,res) =>{
+res.send("Hello user");
+})
+
+app.get('/greet',(req,res) =>{
+    res.send("Good morning sir");
+    })
+app.listen(3000,() =>{
+    console.log('server listen to port 3000'); 
+});
