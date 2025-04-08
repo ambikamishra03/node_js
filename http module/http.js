26157f52ed8f3ef3c43bada90c9ec1c6f674d78e
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-04-08 16:04:30 +0530

    created server

--- http module/http.js
+++ http module/http.js
@@ -5,7 +5,12 @@ const server = http.createServer((req,res) =>{
    res.end();
 })
 // http://127.0.0.1:8080/      go to this , it will show hello world
+
+// server communicates to clients (bring data frm db and provide to client)
+//  server -computer program that is responsible for preparing and 
+// delivering data to other computers(web pages,images,videos etc)
 server.listen(port,(err)=>{
     if(err) console.log(err.message);  
     console.log(`listening to port ${port}`);   
 })
+ 
