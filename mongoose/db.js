d6a96bddbe66992a8b6775b3a2b4642671de2678
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-04-12 13:47:52 +0530

    mongoose and database connection

---
+++ mongoose/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+// define the mongoDB connection url
+
+const mongoUrl = 'mongodb://localhost:27017//hotels'
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+
+const db = mongoose.connection; // this object is what we will use to handle events and interact with database.
+db.on('connected',()=>{
+    console.log('connected to mongoDB server');
+})
+
+db.on('error',()=>{
+    console.log('MongoDB connection error');
+})
+
+db.on('disconnected',()=>{
+    console.log('Disconnected to mongoDB server');
+})
+
+module.exports = db;
