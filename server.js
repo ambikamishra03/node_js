0a28ac3fc6eaf3b966677d315bf1463a4385318d
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-04-13 15:42:49 +0530

    created menu item endpoints and saved data

--- server.js
+++ server.js
@@ -1,37 +1,3 @@
-// srver provide data in form of json
-// json(javascript object notation) - lightweight, structured and organized data.
-//   const jsonString  = '{"name": "Ambika","age": 24,"city":"Deoria"}'
-// const jsonObject = JSON.parse(jsonString)    // convert json string to object
-// console.log(jsonObject);
-
-// const str = JSON.stringify(jsonObject)    // convert object to json string
-// console.log(str);
-
-// console.log(typeof str);
-// console.log(typeof jsonObject);
-
-
-
-// imagine a menu card in restaurant
-// customer - client, waiter - server(expressjs) of restaurant(NodeJS), kitchen room - db, chef - server of kitchen
-// collection of all lists(menu card) - api
-// option in that list - endpoint
-
-
-// Nodejs server responsible for handling HTTP request from client and returning responses.
-// it process these requests, communicate to db server and sends data to client 
-
-
-// creating server in NodeJS using express package
-// localhost - refers to own computer(house of waiter)
-// port number - address of server(room of waiter in that house)
-
-// body parser = middleware library for express.js
-// client sends data to server in form of JSON, form data or url-encoded data.
-// used to parse and extract the body of incoming http request 
-// it gives us data in form which we needed.
-// eg: bodyParser.json() automatically parses json data from request body and convert it 
-// into javascript object, which is then stored in req.body 
 
 
 const express = require('express');
@@ -40,6 +6,7 @@ app.use(express.json());  // ✅ This parses JSON body
 
 const db = require('./db');
 const Person = require('./models/person');
+const MenuItem = require('./models/menu');
 const bodyParser = require('body-parser');
  app.use(bodyParser.json());
 
@@ -48,6 +15,18 @@ const bodyParser = require('body-parser');
 // get method used to request data from server
 app.get('/',(req,res) =>{
 res.send("Hello user");
+})
+
+
+app.get('/person',async (req,res) =>{
+    try{
+      const data = await Person.find();
+      console.log('person data fetched successfully');
+      res.status(200).json(data);
+    }catch{
+        console.log('error while fetching person data');
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
 app.post('/person', (req, res) => {
@@ -64,7 +43,30 @@ app.post('/person', (req, res) => {
             res.status(500).json({ error: 'Internal server error' });
         });
 });
+ 
 
+app.get('/menu', async (req,res) =>{
+  try {
+    const menu = await MenuItem.find();
+    console.log('menu data fetched');
+    res.status(200).json(menu);
+  } catch (error) {
+    console.log('Error while fetching menu item');
+    res.status(500).json({error:'Internal server error'})
+  }
+})
+
+app.post('/menu', (req,res) =>{
+  const data = req.body;
+  const menuItem =  new MenuItem(data);
+   menuItem.save().then(menu =>{
+      console.log('Menu data saved');
+      res.status(200).json(menu);
+   }).catch(error => {
+    console.error('Error saving person', error);
+    res.status(500).json({ error: 'Internal server error' });
+});
+})
 
 app.listen(3000,() =>{
     console.log('server listen to port 3000'); 
