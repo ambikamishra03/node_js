0188f0211c79606f47568485bebf7c9779eb7d57
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-04-21 13:46:54 +0530

    generated tokens

---
+++ jwt.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const jwtAuthMiddleware = (req,res,next) =>{
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) return res.status(401).json({error:"Unauthorized"})
+
+ try {
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+ } catch (error) {
+    console.log(error);
+    res.status(401).json({error:'Invalid token'});
+    
+ }
+}
+
+
+
+function generateToken(userData){
+return jwt.sign(userData, process.env.JWT_SECRET);
+}
+module.exports = {jwtAuthMiddleware,generateToken};
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json()); // ✅ This parses JSON body
 const db = require("./db");
+const dotenv= require('dotenv');
 const bodyParser = require("body-parser");
 const personRoutes = require("./router/personRoutes");
 const menuRoutes = require("./router/menuRoutes");
 const passport = require('./auth') 
 app.use(bodyParser.json());
+dotenv.config();
 
 
 // middleware function 
@@ -27,7 +29,8 @@ app.get("/" ,(req, res) => {
   res.send("Hello user");
 });
 
-app.use("/person",localAuthMiddleware, personRoutes);
+// app.use("/person",localAuthMiddleware, personRoutes);
+app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
 app.listen(3000, () => {
